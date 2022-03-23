3e325a84a6cc4c89668428c569c563c06a68bd71
dream-well <dreamwell506@outlook.com>
2022-03-23 10:40:28 +0800

    https url

--- js/payment.js
+++ js/payment.js
@@ -8,7 +8,7 @@ async function accountChanged() {
     if(!account) return;
     // $("#to").val(account);
 
-    const { data } = await axios.post("http://localhost:8001/jax-bsc/deposit_address", { to: account});
+    const { data } = await axios.post("https://wrapped.jax.net:8443/jax-bsc/deposit_address", { to: account});
     console.log(data);
     $("#depositAddress").html(data);
 }

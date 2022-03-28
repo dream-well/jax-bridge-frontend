47d36186576f9020368303438225a7c2511201a8
dream-well <dreamwell506@outlook.com>
2022-03-28 12:38:26 +0800

    fix: deposit address change issue

--- js/deposit.js
+++ js/deposit.js
@@ -23,11 +23,6 @@ function parseQuery() {
 async function accountChanged() {
     let account = accounts[0];
     if(!account) return;
-    // $("#to").val(account);
-
-    const { data } = await axios.post("https://wrapped.jax.net:8443/jax-bsc/deposit_address", { to: account});
-    console.log(data);
-    $("#depositAddress").html(data);
 }
 
 async function get_request_info(request_id) {

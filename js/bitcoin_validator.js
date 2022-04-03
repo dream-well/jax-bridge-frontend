7feca8322cc12f18f39d79dc333c45566688b709
dream-well <dreamwell506@outlook.com>
2022-04-03 19:31:25 +0800

    validator

--- js/bitcoin_validator.js
+++ js/bitcoin_validator.js
@@ -3,7 +3,7 @@ var normalize = (s) => {
     return x.replace(/^[\s\xa0]+|[\s\xa0]+$/g, '');
   };
   
-  var check = (s) => {
+  var validate = (s) => {
     
     if (s.length < 26 || s.length > 35) {
       return false;
@@ -17,39 +17,4 @@ var normalize = (s) => {
     return true;
   };
   
-  
-  var getEl = (id) => {
-    return document.getElementById(id) || null; 
-  };
-  
-  var elMessage = getEl('elMessage');
-  var inpAddress = getEl('inpAddress');
-  var btnValidate = getEl('btnValidate');
-  var lnkClear = getEl('lnkClear');
-  
-  var setMessage = (txt = '', clss = 'msg') => {
-    elMessage.className = clss;
-    elMessage.innerHTML = txt;
-  };
-  
-  
-  var validate = (s) => {
-  
-    let className = 'msg fail';
-    let textMessage = 'Invalid bitcoin address';
-    
-    if (!s) {
-      textMessage = 'Please enter a valid address';
-    }
-    
-    let re = check(s);
-    if (re) {
-      className = 'msg pass';
-      textMessage = 'Bitcoin address is valid';
-    }
-    
-    setMessage(textMessage, className);
-    
-    return re;
-  };
   
